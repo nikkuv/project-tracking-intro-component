9bf920c1a2e57b5cf2968d4d53377beeb3d3603f
saumya verma <2000vermasaumya18@gmail.com>
2020-08-22 00:49:45 +0530

    push files

---
+++ script.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('#icon-img');
+
+let state = false;
+hamburger.addEventListener('click', function(){
+    const navLinks = document.querySelector('.nav-links');
+    if(!state){
+        navLinks.style.display = 'block';
+        this.src = 'images/icon-close.svg';
+        state = true;
+    }
+    else{
+        navLinks.style.display = 'none';
+        this.src = 'images/icon-hamburger.svg';
+        state = false;
+    }
+    
+});
